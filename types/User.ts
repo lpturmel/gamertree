e75cbecbfb477b332d375a536887444bd14daffb
lpturmel <louisphilippe.turmel@gmail.com>
2021-11-09 00:08:51 -0500

    Removed user.ts

--- types/User.ts
+++
@@ -1,9 +0,0 @@
-import { LeagueEntity } from "./entities/League";
-import { WowEntity } from "./entities/Wow";
-
-export interface User {
-	user_id: string;
-	email: string;
-	username: string;
-	entities: WowEntity | LeagueEntity;
-}
