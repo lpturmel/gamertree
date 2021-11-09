b32ae61bf3f10954c536cb1e5b00a69fdca5f213
lpturmel <louisphilippe.turmel@gmail.com>
2021-11-09 00:13:01 -0500

    Updated Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,21 @@
+import { useUser } from "@auth0/nextjs-auth0";
+import Link from "next/link";
+
 export interface NavbarProps {}
 
 const Navbar: React.FunctionComponent<NavbarProps> = () => {
+	const { user, error, isLoading } = useUser();
+
 	return (
-		<div className="flex w-full">
+		<div className="flex w-full justify-between">
 			<p className="font-bold"> Gamer Tree </p>
+
+			{!isLoading && (
+				<div className="flex flex-row justify-center items-center space-x-4">
+					<img src={user.picture} className="w-8 h-8 rounded-full" />
+					<Link href="/me">{user.name}</Link>
+				</div>
+			)}
 		</div>
 	);
 };
