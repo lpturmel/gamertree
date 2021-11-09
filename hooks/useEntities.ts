23e8a18492e84bd29d87487764d3709b20c4fb03
lpturmel <louisphilippe.turmel@gmail.com>
2021-11-09 00:11:42 -0500

    Added useEntities

---
+++ hooks/useEntities.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+import { Entity } from "../types/Entity";
+
+const useEntities = () => {
+	return useQuery<Entity[]>("entities", async () => {
+		const { data } = await axios.get("/api/entities");
+		return data;
+	});
+};
+
+export default useEntities;
