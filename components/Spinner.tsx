40a99635f57272b0dc1c9a7a26d48ade6da5164e
lpturmel <louisphilippe.turmel@gmail.com>
2021-11-09 00:12:32 -0500

    Added spinner

---
+++ components/Spinner.tsx
@@ -0,0 +1,32 @@
+import { FunctionComponent } from "react";
+
+export interface SpinnerProps {}
+
+const Spinner: FunctionComponent<SpinnerProps> = () => {
+	return (
+		<div className="mx-1 relative h-5 w-5 flex">
+			<svg
+				className="animate-spin absolute text-white top-0"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+			>
+				<circle
+					className="opacity-25"
+					cx="12"
+					cy="12"
+					r="10"
+					stroke="currentColor"
+					strokeWidth="4"
+				></circle>
+				<path
+					className="opacity-75"
+					fill="currentColor"
+					d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+				></path>
+			</svg>
+		</div>
+	);
+};
+
+export default Spinner;
